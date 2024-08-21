c16216d9e8bc67a9e5c856805a551d8457227498
sijirama <gbemilesanmi@gmail.com>
2024-08-21 17:30:18 +0100

    almost done

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Button } from "./components/ui/button"
+
+function App() {
+
+    return (
+        <>
+            <p className='underline text-3xl text-red-500'>
+                hello world
+                <Button>Enter</Button>
+            </p>
+        </>
+    )
+}
+
+export default App
