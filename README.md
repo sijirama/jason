9965797f24339d85ac73704353635c3754cd4bfe
sijirama <gbemilesanmi@gmail.com>
2024-11-26 00:33:48 +0100

    parser skeleton

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 stupid simple json parser in zig
 
 ## important reads i think
+- https://www.ietf.org/rfc/rfc4627.txt
 - https://news.ycombinator.com/item?id=38150833
 - https://notes.eatonphil.com/writing-a-simple-json-parser.html
--- src/parser/main.zig
+++ src/parser/main.zig
@@ -1,4 +1,5 @@
 const Token = @import("../tokenizer/main.zig").Token;
+const TokenType = @import("../tokenizer/main.zig").TokenType;
 const std = @import("std");
 
 //INFO: from the json rfc https://www.ietf.org/rfc/rfc4627.txt
@@ -38,15 +39,86 @@ pub const Parser = struct {
         };
     }
 
-    // Placeholder parsing methods (you'll implement these)
     fn parseObject(self: *Parser) !JsonValue {
-        // TODO: Implement object parsing
-        return error.Unimplemented;
+        const current_token = self.tokens[self.current];
+
+        if (current_token.type != TokenType.LEFT_BRACE) {
+            return error.ExpectedObjectStart;
+        }
+
+        // Move past the left brace
+        self.current += 1;
+
+        const gpa = std.heap.GeneralPurposeAllocator(.{}){};
+        const allocator = gpa.allocator();
+        defer _ = gpa.deinit();
+
+        var map = std.StringHashMap(JsonValue).init(allocator);
+
+        while (current_token.type != .RIGHT_BRACE) {
+            if (self.tokens[self.current].type != .STRING) {
+                return error.ExpectedStringKey;
+            }
+            // Store the key (need to duplicate the string)
+            const key = try allocator.dupe(u8, self.tokens[self.current].value);
+            self.current += 1;
+
+            // Expect a colon
+            if (self.tokens[self.current].type != .COLON) {
+                return error.ExpectedColon;
+            }
+            self.current += 1;
+
+            // Recursively parse the value
+            const value = try self.parseValue();
+
+            // Insert into the hash map
+            try map.put(key, value);
+
+            // Optional comma handling
+            if (self.tokens[self.current].type == .COMMA) {
+                self.current += 1;
+            }
+        }
+
+        // Move past the right brace
+        self.current += 1;
+
+        // Return as a JsonValue Object
+        return JsonValue{ .Object = map };
     }
 
     fn parseArray(self: *Parser) !JsonValue {
-        // TODO: Implement array parsing
-        return error.Unimplemented;
+        //expect left bracket "["
+        const current_token = self.tokens[self.current];
+        if (current_token.type != .LEFT_BRACKET) {
+            return error.ExpectedObjectStart;
+        }
+
+        //move past the left brace
+        self.current += 1;
+
+        const gpa = std.heap.GeneralPurposeAllocator(.{}){};
+        const allocator = gpa.allocator();
+        defer _ = gpa.deinit();
+
+        var array = std.ArrayList(JsonValue).init(allocator);
+
+        while (current_token.type != .RIGHT_BRACKET) {
+            // Parse the value (which could be anything)
+            const value = try self.parseValue();
+
+            // Append to the array
+            try array.append(value);
+
+            // Handle optional comma between elements
+            if (self.tokens[self.current].type == .COMMA) {
+                self.current += 1;
+            }
+        }
+
+        self.current += 1;
+        return JsonValue{ .Array = array.toOwnedSlice() };
     }
 
     fn parseString(self: *Parser) !JsonValue {
