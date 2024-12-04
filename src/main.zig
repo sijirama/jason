de21ee3b63a1778d3aea074862bbef2e0debf825
sijirama <gbemilesanmi@gmail.com>
2024-12-04 16:28:43 +0100

    fix: core structuring idfk

--- src/main.zig
+++ src/main.zig
@@ -3,13 +3,16 @@ const print = std.debug.print;
 const Jason = @import("core/main.zig").Jason;
 
 pub fn main() !void {
-    const path = "./examples/3.json";
+    const path = "./examples/4.json";
     const allocator = std.heap.page_allocator;
 
     const input = try std.fs.cwd().readFileAlloc(allocator, path, 1024);
     defer allocator.free(input); // Free memory after usage
 
     var jason = Jason.init(allocator, input);
-    const queried = try jason.query("$.store.book[3].title");
+
+    const queried = try jason.query("$[1]");
+    //const queried = try jason.query("$.store.book[3].title");
+
     jason.printValue(queried);
 }
