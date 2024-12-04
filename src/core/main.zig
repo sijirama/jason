ed2b7757daa43e207b4571d43aac872c6cbff5c9
sijirama <gbemilesanmi@gmail.com>
2024-12-04 16:27:42 +0100

    fix: core structuring idfk

--- src/core/main.zig
+++ src/core/main.zig
@@ -2,45 +2,47 @@ const std = @import("std");
 const prsr = @import("../parser/main.zig");
 const tkn = @import("../tokenizer//main.zig");
 const qry = @import("../query//tokenizer.zig");
-pub const JsonValue = @import("../parser/main.zig").JsonValue;
+const JsonValue = @import("../parser/main.zig").JsonValue;
 
 pub const Jason = struct {
     allocator: std.mem.Allocator,
+    input: []const u8,
 
-    pub fn init(allocator: std.mem.Allocator) Jason {
+    pub fn init(allocator: std.mem.Allocator, input: []const u8) Jason {
         return Jason{
             .allocator = allocator,
+            .input = input,
         };
     }
-    pub fn parse(self: *Jason, json_string: []const u8) !JsonValue {
 
-        // Tokenize
+    pub fn parse(self: *Jason, json_string: []const u8) !JsonValue {
         var tokenizer = tkn.Tokenizer{
             .input = json_string,
             .position = 0,
         };
 
         const tokens = try tokenizer.tokenize();
-        defer {
-            for (tokens) |token| {
-                self.allocator.free(token.value);
-            }
-            self.allocator.free(tokens);
-        }
 
-        // Parse
         var parser = prsr.Parser.init(tokens, self.allocator);
         defer parser.deinit();
+        const parsed_value = try parser.parse();
 
-        return parser.parse();
+        //prsr.printJsonValue(parsed_value);
+
+        return parsed_value;
     }
 
-    pub fn query(self: *Jason, json_value: JsonValue, query_string: []const u8) !JsonValue {
+    pub fn query(self: *Jason, query_string: []const u8) !JsonValue {
+        const json_value = try self.parse(self.input);
+
         var queryTokenizer = qry.QueryTokenizer.init(query_string, self.allocator);
         defer queryTokenizer.deinit();
 
         try queryTokenizer.scanQueryTokens();
-        return queryTokenizer.executeQuery(json_value);
+
+        const queried_value = try queryTokenizer.executeQuery(json_value);
+        //prsr.printJsonValue(queried_value);
+        return queried_value;
     }
 
     pub fn printValue(self: *Jason, value: JsonValue) void {
--- src/main.zig
+++ src/main.zig
@@ -1,47 +1,15 @@
 const std = @import("std");
-const tkn = @import("tokenizer/main.zig");
-const qrytkn = @import("query/tokenizer.zig");
-const prser = @import("parser/main.zig");
 const print = std.debug.print;
-const jason = @import("core/main.zig");
+const Jason = @import("core/main.zig").Jason;
 
 pub fn main() !void {
     const path = "./examples/3.json";
     const allocator = std.heap.page_allocator;
 
-    // Dynamically allocate memory and read the file
     const input = try std.fs.cwd().readFileAlloc(allocator, path, 1024);
     defer allocator.free(input); // Free memory after usage
 
-    //INFO: TOKENIZER ------------
-
-    const Tokenizer = tkn.Tokenizer;
-    var tokenizer = Tokenizer{
-        .input = input,
-        .position = 0,
-    };
-    const tokens = try tokenizer.tokenize();
-    defer {
-        for (tokens) |token| {
-            std.heap.page_allocator.free(token.value);
-        }
-        std.heap.page_allocator.free(tokens);
-    }
-
-    //INFO: PARSER ------------
-
-    var parser = prser.Parser.init(tokens, allocator);
-    defer parser.deinit();
-    const parsed_value = try parser.parse();
-    //prser.printJsonValue(parsed_value);
-
-    //INFO: QUERY ------------
-
-    var queryTokenizer = qrytkn.QueryTokenizer.init("$.store.book[3].title", allocator);
-    defer queryTokenizer.deinit();
-
-    try queryTokenizer.scanQueryTokens();
-
-    const value = try queryTokenizer.executeQuery(parsed_value);
-    prser.printJsonValue(value);
+    var jason = Jason.init(allocator, input);
+    const queried = try jason.query("$.store.book[3].title");
+    jason.printValue(queried);
 }
